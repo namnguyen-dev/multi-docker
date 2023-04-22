e1d6ebe15179b103689e570592d737049df4cc0e
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2023-04-22 20:21:20 +0700

    set up redit connection

--- server/index.js
+++ server/index.js
@@ -25,6 +25,15 @@ pgClient.on('connect', () => {
     .catch((err) => console.log(err));
 });
 
+// Redis Client Setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+const redisPublisher = redisClient.duplicate();
+
 app.get('/', (req, res) => {
   res.send('Hi');
 });
