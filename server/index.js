dba0f86f0741fc3f24aa38c3ea4ecd50b5caebff
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2023-04-22 20:19:35 +0700

    set up express server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const keys = require('./keys');
+
+// Express App Setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+app.listen(5000, err => {
+  console.log('Listening');
+});
