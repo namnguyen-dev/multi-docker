1ffb7604348f8b76762c8ca6d72d74d6fb594f22
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2023-04-22 20:41:55 +0700

    set up react app, add OtherPage.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const pgClient = new Pool({
 pgClient.on('connect', () => {
   pgClient
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 });
 
 // Redis Client Setup
@@ -34,8 +34,36 @@ const redisClient = redis.createClient({
 });
 const redisPublisher = redisClient.duplicate();
 
+// Express route handlers
+
 app.get('/', (req, res) => {
   res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
+
+  res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
+
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  res.send({ working: true });
 });
 
 app.listen(5000, err => {
